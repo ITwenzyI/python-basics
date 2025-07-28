1f5a08390aff98218b00aedd8fe016f22d01fd08
ItwenzyI <kischoenhaar@gmail.com>
2025-07-28 22:45:19 +0200

    Add Rest, Potenz, Divi mit int, Wurzel

--- zahlen.py
+++ zahlen.py
@@ -2,5 +2,9 @@ print(10+2)
 print(10-2)
 print(10*2)
 print(10/2)
+print(10%3) # Rest
+print(10**3) # Potenz mit ^3
+print(10//6) # Dividieren mit int
+print(10**0.5) # Wurzel
 
-print(type(10))
+print(type(10 / 2))
