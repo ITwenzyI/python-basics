ce469d2e52d650e1a112a0529ed99f810c7ea6bc
ItwenzyI <kischoenhaar@gmail.com>
2025-07-28 10:36:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# String Indexing
+berufsfeld = "IT"
+print("Ich habe schon Erfahrung im Bereich " + berufsfeld)
+print(berufsfeld[-1]) # print(berufsfeld[0])
+
+# String Slicing
+x = "abcdefg hijk"
+print(x[:9]) # print(x[1:9])
+
+# String Länge
+y = "Wie lange bin ich?"
+print(len(y))
